86579338f0268eaaf1c50e6de1bef2441466d841
Leifur Sigurdsson <leifurerik@gmail.com>
2018-11-15 18:02:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,9 +45,9 @@ class Student
   end
 
   def self.new_from_db(row)
-    pat.id = row[0]
-    pat.name = row[1]
-    pat.grade = row[2]
+    id = row[0]
+    name = row[1]
+    grade = row[2]
     self.new(id, name, grade)
   end
 
